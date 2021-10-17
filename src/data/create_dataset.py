aff37bd24c7098aad4db62c6a9122b4bd9b51391
oliveristadfunch <oliver.funch@cognite.com>
2021-10-17 22:05:35 +0200

    Upload from rig

--- src/data/create_dataset.py
+++ src/data/create_dataset.py
@@ -4,19 +4,42 @@ from numpy import meshgrid, linspace,ravel, ones
 
 
 def load_data(fileName, x_dp, y_dp):
+    '''
+    Load 'fileName' and
+    extract section (0,0) to (x_dp,y_dp) from fileName
+    '''
     return imread(fileName)[:y_dp,:x_dp]
 
 def create_normalized_meshgrid(x_dp,y_dp):
+    '''
+    Creates a meshgrid with x and y between 0 and 1.
+    If input coordinates are unequal(non-square meshgrid),
+    meshgrid is normalized to the axis with longest range.
+    
+    Example:
+    x_dp = 10, y_dp = 10 -> x and y is interval [0,1]
+
+    x_dp = 10, y_dp = 5 -> x is interval [0,1]
+                           y is interval [0,0.5]
+
+    x_dp = 5, y_dp = 10 -> x is interval [0,0.5]
+                           y is interval [0,1]
+    '''
     norm = max(x_dp,y_dp)
     return meshgrid(linspace(0,x_dp/norm, x_dp),linspace(0,y_dp/norm, y_dp))
 
 def create_poly_design_matrix(x,y, degree):
+    '''
+    Creates a polynomial features design matrix with a certain degree.
+    With degree 2 the polynomial features are [1, x, y, x^2, xy, y^2]
+    '''
     #Ensure arrays are flattened
     if len(x.shape) > 1:
         x = ravel(x)
     if len(y.shape) > 1:
         y = ravel(y)
-        
+
+    #Total number of datapoints   
     N = len(x)
     l = int((degree+1)*(degree+2)/2)   # Number of elements in beta
     X = ones((N,l))
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -4,24 +4,25 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from numpy import max,min
 
-def plot_3Dsurface(x,y,z,x_dp,y_dp):
-    
+def plot_surf(x,y,z,x_dp,y_dp):
+    '''
+    Plots the surface defined by the coordinates x,y,z.
+    Assumes x and y is or has been a meshgrid.
+    If not meshgrid, reshape back to original shape. 
+    '''
     #Ensure flattened array
-    if(len(x.shape)<2):
+    if(x.shape!=(y_dp,x_dp)):
         x = x.reshape(y_dp,x_dp)
-    if(len(y.shape)<2):
+    if(y.shape!=(y_dp,x_dp)):
         y = y.reshape(y_dp,x_dp)       
-    if(len(z.shape)<2):
+    if(z.shape!=(y_dp,x_dp)):
          z = z.reshape(y_dp,x_dp)  
 
-
-
-    #If x is given as a design matrix
 
     fig = plt.figure()
     fig.set_size_inches(10,10)
     ax = fig.add_subplot(111, projection='3d')
-    #ax = fig.gca(projection='3d')
+
 
     surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
                            linewidth=0, antialiased=True)
@@ -29,8 +30,8 @@ def plot_3Dsurface(x,y,z,x_dp,y_dp):
     
     ax.zaxis.set_major_locator(LinearLocator(8))
     ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-    #ax.zaxis.set_ticks_position('top')
     plt.setp(ax.zaxis.get_majorticklabels(), ha='left')
+
     ax.set_xlabel('X',fontsize=15)
     ax.set_ylabel('Y',fontsize=15)
     ax.set_zlabel('height',labelpad=25,fontsize=15)
@@ -42,14 +43,25 @@ def plot_3Dsurface(x,y,z,x_dp,y_dp):
     fig.colorbar(surf, shrink=0.5, aspect=15,pad = 0.2)    
     return fig
 
+def plot_surf_from_X(X,z,x_dp,y_dp):
+    '''
+    Plotting surface directly from degisn matrix.
+    Assumes the feature x is along column 1 of X, and feature y is along column 2.
+    '''
+    x = X[:,1]
+    y = X[:,2]
+    return plot_surf(x,y,z, x_dp,y_dp)
+
+
 def plot_colormap(z,x_dp,y_dp,color):
+    '''
+    Plots the colormap of the selected terrain area
+    '''
     fig, ax = plt.subplots()
     fig.suptitle('Terrain over Norway 1')
     im = ax.imshow(z.reshape(y_dp,x_dp), cmap=color)
 
     fig.colorbar(im, shrink=1, aspect=15) 
-    im.set_clim(-500,2000)
-    #ax.plot(z.reshape(y_dp,x_dp))
     plt.xlabel = 'X'
     plt.ylabel = 'Y'
     return fig
